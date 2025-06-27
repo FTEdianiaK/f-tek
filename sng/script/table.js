be1bf93970da00d7ad3f3e91a2eb2149f3b7fdb3
Foxie EdianiaK <edaguncz@gmail.com>
2025-06-27 13:00:21 +0200

    :bookmark: 2025-06-27-13-00-21

--- sng/script/table.js
+++ sng/script/table.js
@@ -1,31 +1,3 @@
-function alignTables() {
-    let i, j, k;
-    const tables = document.getElementsByTagName('table');
-    for (i = 0; i < tables.length; i++) {
-        tables[i].className = 'w3-table-all w3-text-black';
-        tables[i].style.width = '90%';
-        tables[i].style.margin = 'auto';
-
-        const rows = tables[i].rows;
-
-        rows[0].className = 'w3-black';
-        const header = rows[0].getElementsByTagName('th');
-        for (j = 0; j < header.length; j++) {
-            header[j].setAttribute('onclick','sortTable(parentElement.parentElement.parentElement.id,' + j.toString() + ')')
-        }
-        for (j = 1; j < header.length; j++) {
-            header[j].classList.add('w3-right-align');
-        }
-
-        for (j = 1; j < rows.length; j++) {
-            let cells = rows[j].getElementsByTagName('td');
-            for (k = 1; k < cells.length; k++) {
-                cells[k].classList.add('w3-right-align');
-            }
-        }
-    }
-}
-
 function sortTable(t, n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.getElementById(t);
@@ -62,6 +34,4 @@ function sortTable(t, n) {
     }
   }
 }
-
-document.addEventListener('DOMContentLoaded', alignTables());
 
